2085898b5608cd0442626a932d04c950d079c65f
Rattapon Simsri <rattapon.simsri0@gmail.com>
2024-01-09 12:38:49 +0700

    FEAT : create folder testing

---
+++ test/job/job_test.go
@@ -0,0 +1,14 @@
+package job_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateNewJob(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(123, 123, "they should be equal")
+
+}
