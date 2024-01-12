c1184aa2fa757b0167473ef1947c9fa50b7b646f
Rattapon Simsri <rattapon.simsri0@gmail.com>
2024-01-12 12:01:50 +0700

    FEAT : test

---
+++ test/porter/porter_test.go
@@ -0,0 +1,15 @@
+package porter_test
+
+import (
+	"testing"
+
+	"github.com/rattapon001/porter-management/internal/porter/domain"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateNewPorter(t *testing.T) {
+	assert := assert.New(t)
+
+	createdPorter, _ := domain.CreateNewPorter("John Smith", "porter-001")
+	assert.Equal(domain.PorterStatusUnavailable, createdPorter.Status, "created porter status should be available")
+}
