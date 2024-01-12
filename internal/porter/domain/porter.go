f220ca26d79b16e3e6e418d3056e646095ece8cb
Rattapon Simsri <rattapon.simsri0@gmail.com>
2024-01-12 11:45:58 +0700

    REFACTOR : func available and id porter

--- internal/porter/domain/porter.go
+++ internal/porter/domain/porter.go
@@ -1,5 +1,7 @@
 package domain
 
+import "github.com/google/uuid"
+
 type PorterId int64
 type PorterStatus string
 
@@ -10,17 +12,24 @@ const (
 )
 
 type Porter struct {
-	Id         PorterId
+	ID         PorterId
 	Name       string
 	PorterCode string
 	Status     PorterStatus
 }
 
 func CreateNewPorter(name string, porterCode string) (*Porter, error) {
+
+	ID, err := uuid.NewUUID()
+
+	if err != nil {
+		return nil, err
+	}
 	return &Porter{
+		ID:         PorterId(ID.ID()),
 		Name:       name,
 		PorterCode: porterCode,
-		Status:     PorterStatusAvailable,
+		Status:     PorterStatusUnavailable,
 	}, nil
 }
 
@@ -28,7 +37,7 @@ func (p *Porter) AcceptJob() {
 	p.Status = PorterStatusWorking
 }
 
-func (p *Porter) CompleteJob() {
+func (p *Porter) Available() {
 	p.Status = PorterStatusAvailable
 }
 
