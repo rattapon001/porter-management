a1aabdc11975e7f7f976d7d2b7e7da37a83f4122
Rattapon Simsri <rattapon.simsri0@gmail.com>
2024-01-12 12:03:15 +0700

    FIX : porter unit test

--- test/porter/porter_test.go
+++ test/porter/porter_test.go
@@ -10,6 +10,7 @@ import (
 func TestCreateNewPorter(t *testing.T) {
 	assert := assert.New(t)
 
-	createdPorter, _ := domain.CreateNewPorter("John Smith", "porter-001")
+	createdPorter, err := domain.CreateNewPorter("John Smith", "porter-001")
+	assert.Nil(err, "error should be nil")
 	assert.Equal(domain.PorterStatusUnavailable, createdPorter.Status, "created porter status should be unavailable")
 }
