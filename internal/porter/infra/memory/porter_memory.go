3a266c50242619a5c2d0e7d53fd884324b9d26f7
Rattapon Simsri <rattapon.simsri0@gmail.com>
2024-01-14 22:40:22 +0700

    FEAT : impliment memory repo

--- internal/porter/infra/memory/porter_memory.go
+++ internal/porter/infra/memory/porter_memory.go
@@ -16,3 +16,31 @@ func (r *PorterMemoryRepository) Save(porter *domain.Porter) error {
 	r.porters = append(r.porters, porter)
 	return nil
 }
+
+func (r *PorterMemoryRepository) Update(porter *domain.Porter) error {
+	for i, p := range r.porters {
+		if p.ID == porter.ID {
+			r.porters[i] = porter
+			return nil
+		}
+	}
+	return nil
+}
+
+func (r *PorterMemoryRepository) FindAvailablePorter() *domain.Porter {
+	for _, p := range r.porters {
+		if p.Status == domain.PorterStatusAvailable {
+			return p
+		}
+	}
+	return nil
+}
+
+func (r *PorterMemoryRepository) FindByID(id domain.PorterId) *domain.Porter {
+	for _, p := range r.porters {
+		if p.ID == id {
+			return p
+		}
+	}
+	return nil
+}
