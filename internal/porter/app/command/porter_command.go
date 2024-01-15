c95dd11a0dd3f395be7340b1eb7fc6398f308662
Rattapon Simsri <rattapon.simsri0@gmail.com>
2024-01-15 11:30:47 +0700

    FEAT : test porter allowcate

--- internal/porter/app/command/porter_command.go
+++ internal/porter/app/command/porter_command.go
@@ -1,5 +1,5 @@
 package command
 
 type PorterCommand interface {
-	Execute(event ...interface{}) error
+	Execute(event ...interface{})
 }
---
+++ test/porter/porter_allowcated_use_case_test.go
@@ -0,0 +1,48 @@
+package porter_test
+
+import (
+	"testing"
+
+	"github.com/rattapon001/porter-management/internal/porter/app"
+	"github.com/rattapon001/porter-management/internal/porter/domain"
+	"github.com/rattapon001/porter-management/pkg"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockNotification struct {
+	mock.Mock
+}
+
+func (m *MockNotification) Notify(token string, payload pkg.NotificationPayload) error {
+	args := m.Called(token, payload)
+	return args.Error(0)
+}
+
+func TestPorterAllowcated(t *testing.T) {
+	assert := assert.New(t)
+	mockRepo := new(MockRepository)
+
+	porter, _ := domain.CreatedNewPorter("porter1", "code-001", "token")
+	mockRepo.On("FindAvailablePorter").Return(porter)
+	mockNoti := new(MockNotification)
+	mockNoti.On("Notify", mock.AnythingOfType("string"), mock.AnythingOfType("pkg.NotificationPayload")).Return(nil)
+	porterService := app.PorterServiceImpl{
+		Repo: mockRepo,
+		Noti: mockNoti,
+	}
+	location := domain.Location{
+		From: "A",
+		To:   "B",
+	}
+	patient := domain.Patient{
+		Name: "John Smith",
+		HN:   "HN123",
+	}
+	job := domain.Job{
+		Location: location,
+		Patient:  patient,
+	}
+	err := porterService.PorterAllowcated(job)
+	assert.NoError(err, "should not return an error")
+}
