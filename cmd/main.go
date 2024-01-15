d7e7b5814cb8f59c5ef115bc83f24d24c4fb806d
Rattapon Simsri <rattapon.simsri0@gmail.com>
2024-01-15 15:08:38 +0700

    FEAT : init main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,18 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	job_router "github.com/rattapon001/porter-management/api/v1/routers/job"
+)
+
 func main() {
-	// Create a new job
-	// 1. Create a new job
-	// 2. Save the job to the repository
-	// 3. Publish the job events
+	port := ":8080"
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+	job_router.InitJobRouter(router)
+	router.Run(port)
 }
