eea9b33da93ee7c0dfb29b141eb135077de1582a
Rattapon Simsri <rattapon.simsri0@gmail.com>
2024-01-18 15:14:22 +0700

    FEAT : impliment porter postgres orm repository

--- internal/porter/infra/postgres_orm/repository.go
+++ internal/porter/infra/postgres_orm/repository.go
@@ -1,6 +1,9 @@
 package postgresorm
 
-import "gorm.io/gorm"
+import (
+	"github.com/rattapon001/porter-management/internal/porter/domain"
+	"gorm.io/gorm"
+)
 
 type PostgresOrmRepository struct {
 	db *gorm.DB
@@ -10,4 +13,61 @@ func NewPostgresOrmRepository(db *gorm.DB) *PostgresOrmRepository {
 	return &PostgresOrmRepository{
 		db: db,
 	}
+}
+
+func (r *PostgresOrmRepository) Save(p *domain.Porter) error {
+	var existingPorter domain.Porter
+	err := r.db.Where("id = ?", p.ID).First(&existingPorter).Error
+	if err == gorm.ErrRecordNotFound {
+		// Porter doesn't exist, insert it
+		err = r.db.Create(p).Error
+		if err != nil {
+			return err
+		}
+	} else if err != nil {
+		// Some error occurred during the find operation
+		return err
+	} else {
+
+		err = r.db.Save(p).Error
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// FindAvailablePorter retrieves an available porter from the Postgres ORM repository.
+// It queries the database for a porter with the status set to domain.PorterStatusAvailable.
+// If a matching porter is found, it returns a pointer to the porter.
+// If no porter is found, it returns nil.
+func (r *PostgresOrmRepository) FindAvailablePorter() (*domain.Porter, error) {
+	var porter domain.Porter
+	err := r.db.Where("status = ?", domain.PorterStatusAvailable).First(&porter).Error
+	if err != nil {
+		return nil, err
+	}
+	return &porter, nil
+}
+
+// FindByID retrieves a porter from the database based on the given ID.
+// It returns a pointer to the porter and an error if any occurred.
+func (r *PostgresOrmRepository) FindByID(id domain.PorterId) (*domain.Porter, error) {
+	var porter domain.Porter
+	err := r.db.Where("id = ?", id).First(&porter).Error
+	if err != nil {
+		return nil, err
+	}
+	return &porter, nil
+}
+
+// FindByCode retrieves a porter from the database based on the given code.
+// It returns a pointer to the porter and an error if any occurred.
+func (r *PostgresOrmRepository) FindByCode(code domain.PorterCode) (*domain.Porter, error) {
+	var porter domain.Porter
+	err := r.db.Where("code = ?", code).First(&porter).Error
+	if err != nil {
+		return nil, err
+	}
+	return &porter, nil
 }
