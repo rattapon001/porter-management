5e6fe3e61654295ee9868019e353af2a6de8220f
Rattapon Simsri <rattapon.simsri0@gmail.com>
2024-01-19 15:08:58 +0700

    FEAT : use postgres sql and add kafka

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,28 @@ import (
 	"github.com/gin-gonic/gin"
 	job_router "github.com/rattapon001/porter-management/api/v1/routers/job"
 	porter_router "github.com/rattapon001/porter-management/api/v1/routers/porter"
+	postgresorm "github.com/rattapon001/porter-management/internal/infra/postgres_orm"
 	job_app "github.com/rattapon001/porter-management/internal/job/app"
+	job_domain "github.com/rattapon001/porter-management/internal/job/domain"
 	job_memory "github.com/rattapon001/porter-management/internal/job/infra/memory"
+	job_postgres "github.com/rattapon001/porter-management/internal/job/infra/postgres_orm"
 	porter_app "github.com/rattapon001/porter-management/internal/porter/app"
+	porter_domain "github.com/rattapon001/porter-management/internal/porter/domain"
 	porter_memory "github.com/rattapon001/porter-management/internal/porter/infra/memory"
+	porter_postgres "github.com/rattapon001/porter-management/internal/porter/infra/postgres_orm"
 )
 
 func main() {
+
+	dbConfig := postgresorm.GetPostgresOrmConfig()
+	db, err := postgresorm.NewPostgresOrmDbConnection(dbConfig)
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&job_domain.Job{}, &porter_domain.Porter{})
+
 	port := ":8080"
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
@@ -20,13 +35,13 @@ func main() {
 	})
 
 	// Init Job Router
-	jobRepository := job_memory.NewJobMemoryRepository()
+	jobRepository := job_postgres.NewPostgresOrmRepository(db)
 	publisher := job_memory.NewMockImplimentEventHandler()
 	JobUseCase := job_app.NewJobUseCase(jobRepository, publisher)
 	job_router.InitJobRouter(router, JobUseCase)
 
 	// Init Porter Router
-	porterRepository := porter_memory.NewPorterMemoryRepository()
+	porterRepository := porter_postgres.NewPostgresOrmRepository(db)
 	PorterPublisher := porter_memory.NewMockImplimentEventHandler()
 	PorterUseCase := porter_app.NewPorterUseCase(porterRepository, PorterPublisher)
 	porter_router.InitPorterRouter(router, PorterUseCase)
