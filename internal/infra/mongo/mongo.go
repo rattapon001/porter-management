d5648934e2cac2207f82ac721035f6f363d0ea5f
Rattapon Simsri <rattapon.simsri0@gmail.com>
2024-01-19 10:47:49 +0700

    FIX : remove log

--- internal/infra/mongo/mongo.go
+++ internal/infra/mongo/mongo.go
@@ -2,7 +2,6 @@ package mongo_db
 
 import (
 	"context"
-	"log"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -11,7 +10,6 @@ import (
 func NewMongoDbConnection(config *MongoConfig) (*mongo.Client, error) {
 
 	uri := "mongodb://" + config.User + ":" + config.Password + "@" + config.Host + ":" + config.Port
-	log.Println("Mongo uri :: ", uri)
 
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
--- internal/infra/postgres_orm/postgres.go
+++ internal/infra/postgres_orm/postgres.go
@@ -2,7 +2,6 @@ package postgresorm
 
 import (
 	"fmt"
-	"log"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -10,7 +9,6 @@ import (
 
 func NewPostgresOrmDbConnection(config *PostgresOrmConfig) (*gorm.DB, error) {
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", config.Host, config.Port, config.User, config.Password, config.Database, config.SSLMode)
-	log.Println("dsn : ", dsn)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
