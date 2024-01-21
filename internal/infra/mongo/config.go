26c5970fe2cac8f2d0fbf1603f01f3af5150c391
Rattapon Simsri <rattapon.simsri0@gmail.com>
2024-01-21 23:05:01 +0700

    FEAT : get mongo config from env

--- internal/infra/mongo/config.go
+++ internal/infra/mongo/config.go
@@ -1,5 +1,10 @@
 package mongo_db
 
+import (
+	"os"
+	"sync"
+)
+
 type MongoConfig struct {
 	Host     string
 	Port     string
@@ -8,12 +13,18 @@ type MongoConfig struct {
 	Database string
 }
 
+var mongoConfig *MongoConfig
+var once sync.Once
+
 func GetMongoConfig() *MongoConfig {
-	return &MongoConfig{
-		Host:     "localhost",
-		Port:     "27017",
-		User:     "root",
-		Password: "123456",
-		Database: "porter_management_db",
-	}
+	once.Do(func() {
+		mongoConfig = &MongoConfig{
+			Host:     os.Getenv("DB_HOST"),
+			Port:     os.Getenv("DB_PORT"),
+			User:     os.Getenv("DB_USERNAME"),
+			Password: os.Getenv("DB_PASSWORD"),
+			Database: os.Getenv("DB_DATABASE"),
+		}
+	})
+	return mongoConfig
 }
