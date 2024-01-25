6a3d0282ed0490a70387ca03b9719d70a24744ad
Rattapon Simsri <rattapon.simsri0@gmail.com>
2024-01-25 17:26:10 +0700

    FEAT : mock data equipment

--- internal/job/app/create_job_use_case.go
+++ internal/job/app/create_job_use_case.go
@@ -1,11 +1,17 @@
 package app
 
 import (
+	"log"
+
+	"github.com/google/uuid"
 	"github.com/rattapon001/porter-management/internal/job/domain"
 )
 
 func (s *JobUseCaseImpl) CreateNewJob(location domain.Location, patient domain.Patient) (*domain.Job, error) {
-	job, err := domain.NewJob(location, patient)
+
+	eId := []domain.EquipmentId{domain.EquipmentId(uuid.New().String()), domain.EquipmentId(uuid.New().String())}
+	job, err := domain.NewJob(location, patient, eId)
+	log.Printf("job: %+v", job)
 	if err != nil {
 		return nil, err
 	}
