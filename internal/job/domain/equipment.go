143c97af252dffca60f58f5ab7b44cf4b56a3c08
Rattapon Simsri <rattapon.simsri0@gmail.com>
2024-01-25 17:23:43 +0700

    FIX : spell variable

--- internal/job/domain/equipment.go
+++ internal/job/domain/equipment.go
@@ -3,21 +3,21 @@ package domain
 type EquipmentId string
 
 type Equipment struct {
-	ID    EquipmentId `bson:"_id" gorm:"primaryKey"`
-	JobId JobId       `bson:"job_id" json:"job_id"`
-	Name  string      `bson:"name" json:"name"`
-	Amont int         `bson:"amont" json:"amont"`
+	ID     EquipmentId `bson:"_id" gorm:"primaryKey;type:uuid"`
+	JobId  JobId       `bson:"job_id" json:"job_id"`
+	Name   string      `bson:"name" json:"name"`
+	Amount int         `bson:"amount" json:"amount"`
 }
 
-func NewEquipment(id EquipmentId, jobId JobId, name string, amont int) (*Equipment, error) {
+func NewEquipment(id EquipmentId, jobId JobId, name string, amount int) (*Equipment, error) {
 	return &Equipment{
-		ID:    id,
-		JobId: jobId,
-		Name:  name,
-		Amont: amont,
+		ID:     id,
+		JobId:  jobId,
+		Name:   name,
+		Amount: amount,
 	}, nil
 }
 
 func (e *Equipment) UpdateAmont(amont int) {
-	e.Amont = amont
+	e.Amount = amont
 }
