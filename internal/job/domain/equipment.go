ac29c8a2b156a5c74994a2d5ad52dbfb42714058
Rattapon Simsri <rattapon.simsri0@gmail.com>
2024-01-29 22:20:40 +0700

    FEAT : add equipment id

--- internal/job/domain/equipment.go
+++ internal/job/domain/equipment.go
@@ -1,20 +1,29 @@
 package domain
 
+import "github.com/google/uuid"
+
 type EquipmentId string
 
 type Equipment struct {
-	ID     EquipmentId `bson:"_id" gorm:"primaryKey;type:uuid"`
-	JobId  JobId       `bson:"job_id" json:"job_id"`
-	Name   string      `bson:"name" json:"name"`
-	Amount int         `bson:"amount" json:"amount"`
+	ID          EquipmentId `bson:"_id" gorm:"primaryKey;type:uuid"`
+	EquipmentId EquipmentId `bson:"equipment_id" json:"equipmentId"`
+	JobId       JobId       `bson:"job_id" json:"jobId"`
+	Name        string      `bson:"name" json:"name"`
+	Amount      int         `bson:"amount" json:"amount"`
 }
 
-func NewEquipment(id EquipmentId, jobId JobId, name string, amount int) (*Equipment, error) {
+func NewEquipment(sourceId EquipmentId, jobId JobId, name string, amount int) (*Equipment, error) {
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return nil, err
+	}
+
 	return &Equipment{
-		ID:     id,
-		JobId:  jobId,
-		Name:   name,
-		Amount: amount,
+		ID:          EquipmentId(id.String()),
+		EquipmentId: sourceId,
+		JobId:       jobId,
+		Name:        name,
+		Amount:      amount,
 	}, nil
 }
 
