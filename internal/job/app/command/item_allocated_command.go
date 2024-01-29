b8dbe8a054e15d6ed3a68f2ba211d976a0b64462
Rattapon Simsri <rattapon.simsri0@gmail.com>
2024-01-30 01:20:21 +0700

    FEAT : job item allocate

---
+++ internal/job/app/command/item_allocated_command.go
@@ -0,0 +1,24 @@
+package command
+
+import (
+	"github.com/rattapon001/porter-management/internal/job/app"
+	"github.com/rattapon001/porter-management/internal/job/domain"
+	"github.com/rattapon001/porter-management/pkg"
+)
+
+type ItemAllocateCommand struct {
+	JobsUseCase app.JobUseCase
+}
+
+type ItemAllocateEventPayload struct {
+	ref   string
+	items []domain.Equipment
+}
+
+func (i *ItemAllocateCommand) Execute(event interface{}) {
+	if eventData, ok := event.(pkg.Event); ok {
+		if eventPayload, ok := eventData.Payload.(ItemAllocateEventPayload); ok {
+			i.JobsUseCase.JobAllocate(domain.JobId(eventPayload.ref), eventPayload.items)
+		}
+	}
+}
